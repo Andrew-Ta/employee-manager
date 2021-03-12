cfe563978c859c422c24d6ed9e1ab3bd15987fb0
Andrew Ta <65432747+Andrew-Ta@users.noreply.github.com>
2021-03-11 19:05:17 -0700

    update api url index.js

--- client/js/dashboard/api/index.js
+++ client/js/dashboard/api/index.js
@@ -2,15 +2,19 @@
 // developmentUrl = 'http://localhost:5000/
 
 const api = {
-    getAllDepartments: 'http://localhost:5000/api/departments/',
+    // getAllDepartments: 'http://localhost:5000/api/departments/',
+    getAllDepartments: 'https://employee-manager-dmit.herokuapp.com/api/departments/',
     // Snippet #5
-    updateEmployee: `http://localhost:5000/api/departments/employee/update`,
+    // updateEmployee: `http://localhost:5000/api/departments/employee/update`,
+    updateEmployee: `https://employee-manager-dmit.herokuapp.com/api/departments/employee/update`,
     getDepartment: (name)=>{
-        return `http://localhost:5000/api/departments/name/${name}`
+        // return `http://localhost:5000/api/departments/name/${name}`
+        return `https://employee-manager-dmit.herokuapp.com/api/departments/name/${name}`
     },
     deleteEmployee: (id)=>{
-        return `http://localhost:5000/api/departments/employee/id/${id}` 
+        // return `http://localhost:5000/api/departments/employee/id/${id}` 
+        return `https://employee-manager-dmit.herokuapp.com/api/departments/employee/id/${id}` 
     }
 }
-
+//https://employee-manager-dmit.herokuapp.com/
 export default api
